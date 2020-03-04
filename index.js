82a569ebcdc13ff3e24c5d955437e49d2b38b3ee
Don <don@thegriffins.org>
2020-03-04 06:26:01 -0600

    tweak example app

---
+++ index.js
@@ -0,0 +1,11 @@
+function onLogin() {
+    debugger;
+}
+
+function setup() {
+    const button = document.querySelector('[type="button"]');
+
+    button.addEventListener('click', onLogin);
+}
+
+setup();
